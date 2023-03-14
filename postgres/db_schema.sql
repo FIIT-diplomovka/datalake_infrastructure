d1c222dc4e1c9ca9cc1602e25c14ef4353851a47
Dominik Horvath <dom.horvath17@gmail.com>
2023-03-14 18:40:05 +0100

    db update

--- postgres/db_schema.sql
+++ postgres/db_schema.sql
@@ -21,6 +21,43 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: staging; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.staging (
+    id integer NOT NULL,
+    bucket character varying NOT NULL,
+    object character varying NOT NULL,
+    metadata json,
+    malware boolean
+);
+
+
+ALTER TABLE public.staging OWNER TO postgres;
+
+--
+-- Name: staging_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.staging_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.staging_id_seq OWNER TO postgres;
+
+--
+-- Name: staging_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.staging_id_seq OWNED BY public.staging.id;
+
+
+--
 -- Name: storage; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -114,6 +151,13 @@ ALTER SEQUENCE public.triples_id_seq OWNED BY public.triples.id;
 
 
 --
+-- Name: staging id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.staging ALTER COLUMN id SET DEFAULT nextval('public.staging_id_seq'::regclass);
+
+
+--
 -- Name: storage id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -125,6 +169,14 @@ ALTER TABLE ONLY public.storage ALTER COLUMN id SET DEFAULT nextval('public.stor
 --
 
 ALTER TABLE ONLY public.triples ALTER COLUMN id SET DEFAULT nextval('public.triples_id_seq'::regclass);
+
+
+--
+-- Data for Name: staging; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.staging (id, bucket, object, metadata, malware) FROM stdin;
+\.
 
 
 --
@@ -144,6 +196,13 @@ COPY public.triples (id, subject, predicate, object) FROM stdin;
 
 
 --
+-- Name: staging_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.staging_id_seq', 2, true);
+
+
+--
 -- Name: storage_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -155,6 +214,14 @@ SELECT pg_catalog.setval('public.storage_id_seq', 2, true);
 --
 
 SELECT pg_catalog.setval('public.triples_id_seq', 1, true);
+
+
+--
+-- Name: staging staging_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.staging
+    ADD CONSTRAINT staging_pkey PRIMARY KEY (id);
 
 
 --
